92b8f6d6fdd5f28b6cce3e515f7b9a9b6fc3dc00
yamanayama <ymmtnomail@gmail.cm>
2020-03-22 18:25:38 +0900

    headerコンポーネント整理

--- src/components/organisms/AppHead.svelte
+++ src/components/organisms/AppHead.svelte
@@ -1,10 +1,13 @@
 <script>
-  import AppNav from "../../components/organisms/AppNav.svelte";
-  import AppHeadLogo from "../../components/atoms/AppHeadLogo.svelte";
-
+  //common
   import { css } from "emotion";
   import Color from "../../../static/style/Color.js";
 
+  //compornents
+  import AppNav from "../../components/organisms/AppNav.svelte";
+  import AppHeadLogo from "../../components/atoms/AppHeadLogo.svelte";
+
+  //variables
   export let segment;
 
   //style
